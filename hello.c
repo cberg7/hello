fcdff28fbf96431aef2b8c52b6fe92805cbe6c6c
cberg7 <cberg7@huskers.unl.edu>
2020-08-22 13:36:40 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Cory Berg
+ * Date: 8-17-2020
+ * 
+ * A Simple Hello World Program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    
+    printf("Cory Berg!\n");
+    
+    return 0;
+    
+}
